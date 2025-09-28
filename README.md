898727b34b10730a4b4dae3b813282af347b924e
aislxflames <proarghaop@gmail.com>
2025-09-28 23:52:37 +0530

    test

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# FlameOS - The Future of Linux
+# Copyright (c) 2024 FlameOS Team
+# https://flame-os.github.io
+# Licensed under GPL-3.0
+
 # flameos-core
 
 ## How to "install" flameos-core
--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+# FlameOS - The Future of Linux
+# Copyright (c) 2024 FlameOS Team
+# https://asiraos.github.io
+# Licensed under GPL-3.0
+
 #/bin/bash
 
 cd x86_64
-repo-add flameos-repo.db.tar.gz *.pkg.tar.zst
-rm -rf flameos-repo.db flameos-repo.files
-mv flameos-repo.db.tar.gz flameos-repo.db
-mv flameos-repo.files.tar.gz flameos-repo.files
+repo-add asiraos-core.db.tar.gz *.pkg.tar.zst
+gpg --detach-sign --armor asiraos-core.db.tar.gz
+rm -rf asiraos-core.db asiraos-core.files
+mv asiraos-core.db.tar.gz asiraos-core.db
+mv asiraos-core.files.tar.gz asiraos-core.files
+
+git add .
+git commit -m "AsiraOS repo update"
+git push --set-upstream origin main
